d1df605e7621a9690ecc74eeed837dbe72ee7096
M Gilang Januar <mgilangjanuar@gmail.com>
2025-05-01 13:09:18 +0700

    refactor: update package name and clean up bun.lock and main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 /**
  * ex-router
- * 
+ *
  * @license MIT
  * (c) 2025 Pradeep Kumar
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction...
@@ -45,14 +45,14 @@ async function compile(app: any, prefixUrl: string, dirPath: string, baseRoute: 
   for (const file of files) {
     const filePath = path.join(dirPath, file);
     const stat = await fs.promises.stat(filePath);
-    
+
     if (stat.isDirectory()) {
       await compile(app, prefixUrl, filePath, baseRoute + '/' + file);
     }
     else if (file.endsWith('.ts') || file.endsWith('.js')) {
       await registerFileRoutes(app, prefixUrl, filePath, baseRoute, path.extname(file));
     }
-  
+
   }
 }
 
@@ -82,10 +82,10 @@ async function registerFileRoutes(
     'ALL', 'USE', // express
     'TRACE', 'CONNECT' // fastify
   ];
-  
+
   for (const method of supportedMethods) {
     if (module[method] && typeof app[method.toLowerCase()] === 'function') {
-      app[method.toLocaleLowerCase()](prefixUrl + routePath, module[method])
+      app[method.toLocaleLowerCase()](prefixUrl + routePath, ...Array.isArray(module[method]) ? module[method] : [module[method]])
     }
   }
 }
